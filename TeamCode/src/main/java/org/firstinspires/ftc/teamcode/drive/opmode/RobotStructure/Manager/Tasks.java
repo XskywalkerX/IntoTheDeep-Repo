083ffc9967255699eb53ab0d64c2a03ea18506bb
skywalker <lipeeskywalker@gmail.com>
2024-11-11 17:38:53 -0300

    feat: enum with autonomous tasks

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RobotStructure/Manager/Tasks.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.RobotStructure.Manager;
+
+public enum Tasks {
+    CATCH_SAMPLE("CATCH_SAMPLE"),
+    DELIVERY_BASKET("D_BASKET"),
+    DELIVERY_OBS_ZONE("D_OBS_ZONE"),
+    CLIP_SPECIMEN("CLIP_SPECIMEN"),
+    PARK("PARK");
+    public final String TaskName;
+
+    public static Tasks getTask(String TaskName) {
+        Tasks match = null;
+
+        for (Tasks task : Tasks.values()) {
+            if (task.TaskName.equals(TaskName)) {
+                match = task;
+            }
+        }
+        return match;
+    }
+
+    private Tasks(String name) {
+        this.TaskName = name;
+    }
+}
