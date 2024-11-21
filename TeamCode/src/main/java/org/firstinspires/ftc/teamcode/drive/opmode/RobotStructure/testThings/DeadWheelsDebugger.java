904ca75138ca68f7dc34b24e4d2bcb5898b22cf4
skywalker <lipeeskywalker@gmail.com>
2024-11-21 17:00:50 -0300

    feat: opmode to verify deadwheels outputs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RobotStructure/testThings/DeadWheelsDebugger.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.RobotStructure.testThings;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.util.Encoder;
+
+@TeleOp
+public class DeadWheelsDebugger extends LinearOpMode {
+
+    Encoder front, left, right;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        front = new Encoder(hardwareMap.get(DcMotorEx.class, "backRight"));
+        left = new Encoder(hardwareMap.get(DcMotorEx.class, "motor_linear_front"));
+        right = new Encoder(hardwareMap.get(DcMotorEx.class, "backLeft"));
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+        left.setDirection(Encoder.Direction.FORWARD);
+        right.setDirection(Encoder.Direction.FORWARD);
+        front.setDirection(Encoder.Direction.FORWARD);
+
+        waitForStart();
+        while(opModeIsActive()) {
+
+            telemetry.addData("f", front.getCurrentPosition());
+            telemetry.addData("r", right.getCurrentPosition());
+            telemetry.addData("l", left.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
