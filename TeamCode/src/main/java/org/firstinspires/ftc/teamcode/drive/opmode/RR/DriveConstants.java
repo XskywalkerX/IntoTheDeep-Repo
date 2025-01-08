cfd62048e6d64c2856f36c85c6144199dfa90b0f
skywalker <lipeeskywalker@gmail.com>
2025-01-08 14:30:57 -0300

    feat: use two dead wheels odometry with BNO055IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RR/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RR/DriveConstants.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.RR;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 /*
@@ -47,7 +48,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 14.32; // in
+    public static double TRACK_WIDTH = 14.98; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -87,10 +88,15 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = 73.47;
-    public static double MAX_ACCEL = 73.47;
-    public static double MAX_ANG_VEL = Math.toRadians(236.82178644407097);
+    public static double MAX_VEL = 66.123;
+    public static double MAX_ACCEL = 66.123;
+    public static double MAX_ANG_VEL = Math.toRadians(40);
     public static double MAX_ANG_ACCEL = Math.toRadians(170.08199999999997);
+
+    public static RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR =
+            RevHubOrientationOnRobot.LogoFacingDirection.UP;
+    public static RevHubOrientationOnRobot.UsbFacingDirection USB_FACING_DIR =
+            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
 
     public static double encoderTicksToInches(double ticks) {
