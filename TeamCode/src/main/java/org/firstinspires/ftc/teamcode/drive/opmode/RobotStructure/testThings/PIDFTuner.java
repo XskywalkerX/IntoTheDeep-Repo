8ffec05a6aa4c33779fcf67b0e0bd35b70ce60cb
skywalker <lipeeskywalker@gmail.com>
2025-01-10 09:40:00 -0300

    fix: PID constants updated in the loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RobotStructure/testThings/PIDFTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RobotStructure/testThings/PIDFTuner.java
@@ -1,14 +1,20 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.RobotStructure.testThings;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.acmerobotics.roadrunner.control.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.opmode.RobotStructure.MProfile;
 
 @TeleOp
+@Config
 public class PIDFTuner extends LinearOpMode {
 
     PIDCoefficients pidCoefficients = new PIDCoefficients(kP, kI, kD);
@@ -18,21 +24,33 @@ public class PIDFTuner extends LinearOpMode {
 
     volatile public static DcMotorEx linear;
 
-    volatile public static double kP = 0;
+    public static double TARGET = 0.7;
+
+    volatile public static double kP = 0.05;
     volatile public static double kI = 0;
     volatile public static double kD = 0;
 
-    public static double kV = 0;
-    public static double kA = 0;
+    public static double kV = 0.000111607;
+    public static double kA = 0.0002;
     public static double kStatic = 0;
 
     public static double MAX_VEL = 8960.0;
     public static double MAX_ACCEL = 2000.0;
 
-    volatile public static int tp = 0;
+    volatile public static int tp = 5000;
+
+    Servo arm;
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        linear = hardwareMap.get(DcMotorEx.class, "linear");
+        arm = hardwareMap.get(Servo.class, "arm");
+
+        linear.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
 
         mp = new MProfile(pidCoefficients, pidfController, linear, MAX_VEL, MAX_ACCEL);
 
@@ -41,14 +59,17 @@ public class PIDFTuner extends LinearOpMode {
         waitForStart();
         t1.start();
         while(opModeIsActive()) {
+
+            pidCoefficients = new PIDCoefficients(kP, kI, kD);
+            pidfController = new PIDFController(pidCoefficients, kV, kA, kStatic);
+
+            arm.setPosition(TARGET);
             linear.setTargetPosition(tp);
 
             telemetry.addData("TARGET VEL", mp.getVelocity());
             telemetry.addData("CURRENT VEL", linear.getVelocity());
             telemetry.addData("CURRENT POSITION", linear.getCurrentPosition());
             telemetry.update();
-
-            sleep(150);
         }
         t1.interrupt();
     }
