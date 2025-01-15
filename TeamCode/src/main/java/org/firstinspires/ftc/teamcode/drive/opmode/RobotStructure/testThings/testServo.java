0d6242db9fd2775920be6239157b9ac571c9aa46
skywalker <lipeeskywalker@gmail.com>
2025-01-15 09:35:04 -0300

    test: opmode to find servo arm positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RobotStructure/testThings/testServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RobotStructure/testThings/testServo.java
@@ -1,13 +1,17 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.RobotStructure.testThings;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 @TeleOp
 public class testServo extends LinearOpMode {
 
     Servo arm;
+
+    public static double tp = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -16,12 +20,7 @@ public class testServo extends LinearOpMode {
 
         waitForStart();
         while(opModeIsActive()) {
-            if(gamepad1.a) {
-                arm.setPosition(1.0);
-            }
-            if(gamepad1.b) {
-                arm.setPosition(0.0);
-            }
+            arm.setPosition(tp);
         }
     }
 }
