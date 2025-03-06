920a686feca3c41d7761263656ea3ae2c843bacc
skywalker <lipeeskywalker@gmail.com>
2025-03-06 18:56:48 -0300

    feat: transfer now working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/DeliverySystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/DeliverySystem.java
@@ -6,8 +6,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.enums.ArmIntakeStates;
 import org.firstinspires.ftc.teamcode.enums.ClawStates;
 import org.firstinspires.ftc.teamcode.enums.DeliveryStates;
+import org.firstinspires.ftc.teamcode.enums.IntakeStates;
 import org.firstinspires.ftc.teamcode.enums.VerticalLinearStates;
 
 public class DeliverySystem {
@@ -15,7 +17,7 @@ public class DeliverySystem {
     DeliveryClaw deliveryClaw;
     VerticalLinear verticalLinear;
 
-    double systemDelay = 0.5;
+    double systemDelay = 1.5;
 
     public static DeliveryStates CS = DeliveryStates.INITIALIZE, PS = DeliveryStates.INITIALIZE;
 
@@ -35,21 +37,32 @@ public class DeliverySystem {
     public void update(Robot robot, Telemetry telemetry, double manualPower) {
         switch (CS) {
             case INITIALIZE:
+                VerticalLinear.CS = VerticalLinearStates.PREVENT_STATE;
+                CS = DeliveryStates.TRANSFER;
             case TRANSFER:
-                VerticalLinear.CS = VerticalLinearStates.TRANSFER;
+                if (PS != CS) {
+                    VerticalLinear.CS = VerticalLinearStates.PREVENT_STATE;
+                    time.reset();
+                }
+
+                if (time.seconds() >= 2.5) {
+                    VerticalLinear.CS = VerticalLinearStates.TRANSFER;
+                }
+
                 if (PS != DeliveryStates.TRANSFER && PS != DeliveryStates.INITIALIZE) {
                     time.reset();
                 }
                 DeliveryClaw.CS = ClawStates.OPENED;
+
                 robot.deliveryX.setPosition(0);
 
                 if (time.seconds() >= systemDelay) {
                     robot.deliveryY.setPosition(0.68);
                     time.reset();
                 }
-            break;
+                break;
             case SPECIMEN_INTAKE:
-                VerticalLinear.CS = VerticalLinearStates.RETRACTED;
+
                 if (PS != DeliveryStates.SPECIMEN_INTAKE) {
                     time.reset();
                     DeliveryClaw.CS = ClawStates.OPENED;
@@ -58,6 +71,7 @@ public class DeliverySystem {
                 robot.deliveryY.setPosition(0.4);
                 if (time.seconds() >= systemDelay) {
                     robot.deliveryX.setPosition(0.93);
+                    VerticalLinear.CS = VerticalLinearStates.RETRACTED;
                     time.reset();
                 }
                 break;
@@ -65,8 +79,13 @@ public class DeliverySystem {
                 VerticalLinear.CS = VerticalLinearStates.CLIP_SPECIMEN;
                 if (PS != DeliveryStates.SPECIMEN_OUTTAKE_1) {
                     time.reset();
+                    IntakeClaw.CS = ClawStates.OPENED;
+                }
+
+                if (time.seconds() >= 0.5 && time.seconds() <= 0.9) {
                     DeliveryClaw.CS = ClawStates.CLOSED;
                 }
+
                 if (time.seconds() >= systemDelay) {
                     robot.deliveryX.setPosition(0.3);
                     time.reset();
@@ -74,16 +93,26 @@ public class DeliverySystem {
                 robot.deliveryY.setPosition(0.1);
                 break;
             case HIGH_BASKET:
-                VerticalLinear.CS = VerticalLinearStates.HIGH_BASKET;
-                if (PS != DeliveryStates.HIGH_BASKET) {
-                    time.reset();
-                    DeliveryClaw.CS = ClawStates.CLOSED;
+                if (IntakeSystem.CS == IntakeStates.IDLE && ArmIntakeSystem.CS == ArmIntakeStates.TRANSFER && VerticalLinear.CS == VerticalLinearStates.TRANSFER) {
+                    if (PS != DeliveryStates.HIGH_BASKET) {
+                        time.reset();
+                        IntakeClaw.CS = ClawStates.OPENED;
+                    }
+                    if (time.seconds() >= 0.5 && time.seconds() <= 0.9) {
+                        DeliveryClaw.CS = ClawStates.CLOSED;
+                    }
+
+                    if (time.seconds() >= systemDelay) {
+                        VerticalLinear.CS = VerticalLinearStates.HIGH_BASKET;
+                        if (time.seconds() >= systemDelay) {
+                            robot.deliveryX.setPosition(0.3);
+                            time.reset();
+                        }
+                        robot.deliveryY.setPosition(0);
+                    }
+                } else {
+                    IntakeSystem.CS = IntakeStates.IDLE;
                 }
-                if (time.seconds() >= systemDelay) {
-                    robot.deliveryX.setPosition(0.3);
-                    time.reset();
-                }
-                robot.deliveryY.setPosition(0);
         }
 
         //robot.leftLinear.setPower(manualPower * 0.9);
