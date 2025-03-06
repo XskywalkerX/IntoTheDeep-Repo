2bf87314a154afd28ef6f59657794ad9846f6380
skywalker <lipeeskywalker@gmail.com>
2025-03-06 18:58:50 -0300

    feat: Vision Portal implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -7,6 +9,10 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
 
 public class Robot {
     public DcMotorEx horizontalLinear;
@@ -17,6 +23,9 @@ public class Robot {
     public Servo deliveryClaw, deliveryX, deliveryY;
 
     public WebcamName webcam;
+
+    public ColorBlobLocatorProcessor colorLocator;
+    public VisionPortal portal;
 
 
 
@@ -40,9 +49,6 @@ public class Robot {
         leftLinear.setDirection(DcMotorSimple.Direction.REVERSE);
         rightLinear.setDirection(DcMotorSimple.Direction.FORWARD);
 
-
-
-
         intakeClaw = hwMap.get(Servo.class, "claw");
         clawB = hwMap.get(Servo.class, "clawB");
         intakeX = hwMap.get(Servo.class, "itkX");
@@ -51,5 +57,20 @@ public class Robot {
         deliveryClaw = hwMap.get(Servo.class, "clawD");
         deliveryY = hwMap.get(Servo.class, "delY");
         deliveryX = hwMap.get(Servo.class, "delX");
+
+        colorLocator = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(ColorRange.BLUE)         // use a predefined color match
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)    // exclude blobs inside blobs
+                .setRoi(ImageRegion.asUnityCenterCoordinates(-0.9, 0.9, 0.9, -0.9))  // entire frame
+                .setDrawContours(true)                        // Show contours on the Stream Preview
+                .setBlurSize(5)                               // Smooth the transitions between different colors in image
+                .build();
+
+        portal = new VisionPortal.Builder()
+                .addProcessor(colorLocator)
+                .setCameraResolution(new Size(320 * 2, 240 * 2))
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
+                .setCamera(webcam)
+                .build();
     }
 }
